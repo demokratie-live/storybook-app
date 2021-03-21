174892259190aeebb6513a9e99f61737a1ea220b
ManAnRuck <github@manuelruck.de>
2021-03-21 20:09:45 +0100

    Start/Colors add background
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- packages/ui/src/Start/Colors/Colors.stories.tsx
+++ packages/ui/src/Start/Colors/Colors.stories.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { Colors } from './Colors';
+import { Background } from '../../decorators/Background';
 
-storiesOf('Start', module).add('colors', () => <Colors />);
+storiesOf('Start', module).add('colors', () => (
+  <Background>
+    <Colors />
+  </Background>
+));

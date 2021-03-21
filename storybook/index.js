14ec58d543dc81859dbf68145df5207dc3b41ed1
ManAnRuck <github@manuelruck.de>
2021-03-21 09:07:08 +0100

    fix storybook web server stories list
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- storybook/index.js
+++ storybook/index.js
@@ -1,12 +1,11 @@
-// if you use expo remove this line
 import { AppRegistry } from 'react-native';
-
 import {
   getStorybookUI,
   configure,
   addDecorator,
 } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
+import { host, port } from './config.json';
 
 import './rn-addons';
 
@@ -20,7 +19,12 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+  // `host` and `port` are dynamically generated from storybook.sh
+  host,
+  port,
+});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
